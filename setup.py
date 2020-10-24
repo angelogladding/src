80bb69271177e983a07ece43b030d60997e65b8f
Gaea <gaea@lahacker.net>
2020-10-24 01:24:51 +0000

    Bootstrap

---
+++ setup.py
@@ -0,0 +1,28 @@
+# [`src`][1] Copyright @[Angelo Gladding][2] 2020-
+#
+# This program is free software: it is distributed in the hope that it
+# will be useful, but *without any warranty*; without even the implied
+# warranty of merchantability or fitness for a particular purpose. You
+# can redistribute it and/or modify it under the terms of the @@[GNU's
+# Not Unix][3] %[Affero General Public License][4] as published by the
+# @@[Free Software Foundation][5], either version 3 of the License, or
+# any later version.
+#
+# *[GNU]: GNU's Not Unix
+#
+# [1]: //angelogladding.com/code/src
+# [2]: //angelogladding.com
+# [3]: //gnu.org
+# [4]: //gnu.org/licenses/agpl
+# [5]: //fsf.org
+
+"""Tools for metamodern software development."""
+
+from setuptools import setup
+
+setup(requires=["flake8", "flake8-docstring", "ghdiff", "gnupg", "ipython",
+                "pep8-naming", "pss", "pytest", "pytest_cov", "pytest_pep8",
+                "sh"],
+      provides={"kt.extensions": ["sloc = src:count_sloc"],
+                "term.apps": ["src = src.__main__:main"]},
+      discover=__file__)
